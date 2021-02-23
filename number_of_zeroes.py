03d26a6cf1c1c32e1203fc52ef1719f8452c3b92
Chemarmazovich <pasta.chemarmazovich@mail.ru>
2021-02-23 22:53:42 +0300

    Fix problems with user input. Edit code according to style

--- number_of_zeroes.py
+++ number_of_zeroes.py
@@ -1,13 +1,19 @@
 # Дано натуральное число. Найти количество нулей среди цифр числа
 import sys
 
-num = int(sys.argv[1])
+try:
+    num = int(sys.argv[1])
 
-counter = 0;
+    if num > 0:
+        counter = 0
 
-while num > 0:
-    if (num%10) == 0:
-        counter = counter + 1
-    num = num // 10
+        while num > 0:
+            if num % 10 == 0:
+                counter = counter + 1
+            num = num // 10
 
-print("Number of zeroes: " + str(counter))
+        print("Number of zeroes: " + str(counter))
+    else:
+        print("Natural number is expected instead of: " + str(num))
+except ValueError:
+    print("Invalid input")
